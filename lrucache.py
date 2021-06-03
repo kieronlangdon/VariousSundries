d504f14cd4b5d300b38f45f6fcafec35337a513f
kieronlangdon <kieronlangdon@gmail.com>
2021-06-03 14:43:34 +0100

    Create lrucache.py

---
+++ lrucache.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=5)
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+def main():
+    for i in range(400):
+        print(i, fib(i))
+    print("done")
+
+
+if __name__ == '__main__':
+    main()
